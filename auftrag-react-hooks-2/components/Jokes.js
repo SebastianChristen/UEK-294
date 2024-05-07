419b73f44c9baa0841b471ec25a0dd105c52ff0c
Sebastian Christen <bchris@bbcag.ch>
2024-05-07 16:37:57 +0200

    add jokes

---
+++ auftrag-react-hooks-2/components/Jokes.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function GetRandomJoke() {
+  const [joke, setjoke] = useState([]);
+  const [jokeVar, setjokeVar] = useState(0);
+
+  const URL = `https://api.dadjokes.io/api/random/joke`;
+
+  useEffect(() => {
+    const loadjoke = async () => {
+      const response = await fetch(URL);
+      const joke = await response.json();
+      console.log(response);
+      setjoke(joke.body);
+    };
+    loadjoke();
+  }, [jokeVar]);
+
+  //Sobald sich diese var ändert, wird der useEffect aufgerufen. UseEffect == Observable
+  const getJoke = (e) => {
+    setjokeVar(jokeVar + 1);
+  };
+
+  return (
+    <div>
+      {joke.map((joke) => {
+        return (
+          <div key={joke._id}>
+            <div>
+              <h2>{joke.setup}</h2>
+
+              <p>{joke.punchline}</p>
+
+              <button onClick={getJoke}>get joke</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- auftrag-react-hooks-2/pages/index.js
+++ auftrag-react-hooks-2/pages/index.js
@@ -1,10 +1,13 @@
 import Counter from "../components/ColoredCounter";
 import HogwartsHouse from "../components/HogwartsHouse";
+import GetRandomJoke from "../components/Jokes";
 
 export default function IndexPage() {
   return (
     <div>
-      <Counter></Counter>
+      <GetRandomJoke></GetRandomJoke>
+
+     <Counter></Counter>
 
       <HogwartsHouse name="Gryffindor" />
     </div>
