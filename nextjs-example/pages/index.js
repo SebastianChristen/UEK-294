148c079cefce1941e5d862a141f88bbd4963b5dd
Sebastian Christen <bchris@bbcag.ch>
2024-05-13 14:18:25 +0200

    remove examples

--- nextjs-example/pages/index.js
+++
@@ -1,9 +0,0 @@
-import styles from "./index.module.css"
-
-export default function IndexPage() {
-    return (
-        <div className={styles.index}>
-            <h1>Index Page</h1>
-        </div>
-    )
-}
--- static-webpage/script.js
+++
@@ -1,2 +0,0 @@
-// Weitere console Befehle: https://developer.mozilla.org/en-US/docs/Web/API/console
-console.log("Hello World!")
